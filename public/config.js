1507b7fb50fcaf6568d43f0c20254fbe8bb622c9
wuxie136 <105388558+wuxie136@users.noreply.github.com>
2022-09-19 14:31:52 +0800

    Add files via upload

---
+++ public/config.js
@@ -0,0 +1,33 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: '网站监控',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur1754894-a444783ef0c1a99577564dbf',
+  ],
+
+  // 日志天数
+  CountDays: 60,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: '个人主页',
+      url: 'https://www.alm5.cn/'
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/yb/uptime-status'
+    },
+    {
+      text: 'Blog',
+      url: 'https://blog.alm5.cn/'
+    },
+  ],
+};
---
+++ src/components/app.js
@@ -0,0 +1,34 @@
+import { useMemo } from 'react';
+import Link from './link';
+import Header from './header';
+import UptimeRobot from './uptimerobot';
+import Package from '../../package.json';
+
+function App() {
+
+  const apikeys = useMemo(() => {
+    const { ApiKeys } = window.Config;
+    if (Array.isArray(ApiKeys)) return ApiKeys;
+    if (typeof ApiKeys === 'string') return [ApiKeys];
+    return [];
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <div className='container'>
+        <div id='uptime'>
+          {apikeys.map((key) => (
+            <UptimeRobot key={key} apikey={key} />
+          ))}
+        </div>
+        <div id='footer'>
+          <p>基于 <Link to='https://uptimerobot.com/' text='UptimeRobot' /> 接口制作，检测频率 5 分钟</p>
+          <p>&copy; 2020-2022 <Link to='https://www.alm5.cn' text='林木' />, Version {Package.version}</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
